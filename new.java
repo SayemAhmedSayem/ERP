191152ef9caf84c7a97ceafe3bdf2fe202e24c14
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-02 22:35:15 +0600

    Update new.java

--- new.java
+++ new.java
@@ -9,3 +9,7 @@ public static void main(String[] args) {
 }
 
 System.out.println("Java is fun!")
+
+System.out.println("Hello World!");
+System.out.println("I am learning Java.");
+System.out.println("It is awesome!");
