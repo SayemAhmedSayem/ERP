78fc22354fbaa3208cf4d0145565eed17840b966
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-19 00:23:57 +0600

    Update new.java
    You can also use the println() method to print numbers.
    
    However, unlike text, we don't put numbers inside double quotes

--- new.java
+++ new.java
@@ -16,3 +16,6 @@ System.out.println("It is awesome!");
 
 System.out.println("This sentence will work!");
 System.out.println(This sentence will produce an error);
+System.out.println(3);
+System.out.println(358);
+System.out.println(50000);
