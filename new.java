5c83a4916e99388354f175a15724b517e058c726
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-19 23:59:22 +0600

    Update new.java

--- new.java
+++ new.java
@@ -19,3 +19,5 @@ System.out.println(This sentence will produce an error);
 System.out.println(3);
 System.out.println(358);
 System.out.println(50000);
+System.out.println(3 + 3);
+System.out.println(2 * 5);
